39bbe5952ae0c8065d251a344d1479bdc013c59f
Zsolt Takács <zsolt@takacs.cc>
2013-08-25 10:53:24 +0200

    Add user parameter to vagrant_plugin resource

--- resources/plugin.rb
+++ resources/plugin.rb
@@ -6,3 +6,4 @@ attribute :plugin_name, :name_attribute => true
 attribute :version, :kind_of => [String]
 attribute :installed, :kind_of => [TrueClass, FalseClass]
 attribute :installed_version, :kind_of => [String]
+attribute :user, :kind_of => [String]
