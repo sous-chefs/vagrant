2eaee9e72b23982e5dd74a212a3d27c9dc9fb1f4
Patrick Connolly <patrick@myplanetdigital.com>
2014-03-24 16:49:19 -0700

    Set attrs for default vagrant_plugin array set in plugins attr. Add docs.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,9 @@
 default['vagrant']['url'] = nil
 default['vagrant']['checksum'] = nil
-default['vagrant']['plugins'] = []
 default['vagrant']['msi_version'] = ""
+default['vagrant']['plugins'] = []
+
+# Set user and home resource attrs for default plugins installed from
+# node['vagrant']['plugins'] array.
+default['vagrant']['user'] = nil
+default['vagrant']['home'] = nil
