e75f3556c7cc60f59b1c5180e7956c4a5aff110a
Ty Alexander <ty.alexander@sendgrid.com>
2015-01-08 13:37:57 -0700

    Update berksfile to support berkshelf-api source URL
    Fix kitchen converge for centos.  The ohai value for platform_family appears to have changed from "redhat" to "rhel".
    
    Update vagrant version to 1.7.2, minor version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.3.0:
+
+* Update vagrant to the 1.7.2 version (from 1.6.5)
+* Change platform_family check from "redhat" to "rhel"
+  to make `kitchen converge centos` correctly complete.
+
 ## 0.2.0:
 
 * Add `uninstall_gem` recipe to remove vagrant (1.0) gem.
--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-default['vagrant']['version']     = '1.6.5'
+default['vagrant']['version']     = '1.7.2'
 default['vagrant']['url']         = vagrant_package_uri(node['vagrant']['version'])
 default['vagrant']['checksum']    = vagrant_sha256sum(node['vagrant']['version'])
 default['vagrant']['plugins']     = []
