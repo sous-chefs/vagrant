e5b6087f1ef9ab2fc66d356812b48eac7a1993af
jtimberman <joshua@chef.io>
2015-01-09 10:54:48 -0700

    release v0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.2:
+
+* Fix platform_family, redhat is not a family, rhel is. (#18)
+
 ## 0.2.0:
 
 * Add `uninstall_gem` recipe to remove vagrant (1.0) gem.
--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,12 @@ maintainer       'Joshua Timberman'
 maintainer_email 'cookbooks@housepub.org'
 license          'Apache 2.0'
 description      'Installs/Configures vagrant'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 supports 'debian', '>= 6.0'
 supports 'ubuntu', '>= 12.04'
 supports 'redhat', '>= 6.3'
 supports 'centos', '>= 6.4'
 
-depends "dmg"
-depends "windows"
+depends 'dmg'
+depends 'windows'
